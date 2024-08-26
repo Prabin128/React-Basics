af37529e0e46aad1e48d71e4c6e36a4edb43cae1
Prabin <poudelprabin09@gmail.com>
2024-08-27 00:47:15 +0545

    babel and JSX

--- 2.Introduction_to_JSX_and_babel/my-react-app/src/App.jsx
+++ 2.Introduction_to_JSX_and_babel/my-react-app/src/App.jsx
@@ -1,34 +1,18 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
-import './App.css'
+import './App.css'   //This imports the CSS file App.css, which is used to apply styles specific to the App component.
 
+//This defines a functional React component named App. A React component is essentially a JavaScript function that returns JSX.
 function App() {
-  const [count, setCount] = useState(0)
 
-  return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+          /*This is where JSX comes into play. 
+          JSX is a syntax extension that looks similar to HTML but is actually JavaScript. 
+          It allows us to write HTML-like syntax directly in our JavaScript files.
+          In this case, <h1>hello world</h1> is JSX. 
+          When the App component is rendered, this JSX is converted to a React element that the browser can understand and render as HTML.*/
+  return ( 
+    <h1>hello world</h1>  //Hence, inside a js file , we are able to write plain html. This is JSX  does.React works by creating this JSX files.
   )
 }
 
--- 2.Introduction_to_JSX_and_babel/my-react-app/src/main.jsx
+++ 2.Introduction_to_JSX_and_babel/my-react-app/src/main.jsx
@@ -1,10 +1,27 @@
-import { StrictMode } from 'react'
+/* `StrictMode` is a wrapper component provided by React that helps to identify potential problems in an application. 
+It doesn't render any visible UI but activates additional checks and warnings for its children in development mode. */
+import { StrictMode } from 'react'   
+
+/* `createRoot` is the method used to create a root React node. 
+This method is part of the new React 18 API, which allows you to manage the rendering process more efficiently. */
 import { createRoot } from 'react-dom/client'
+
+/*This imports the App component from the App.jsx file. The App component is the main component of your application that gets rendered.*/
 import App from './App.jsx'
+
 import './index.css'
 
+
+
+/*
+                                            createRoot(document.getElementById('root')).render(...):
+
+    - createRoot(document.getElementById('root')) creates a root where the React component tree will be rendered.
+    - .render(...) renders the React component (in this case, <App />) inside the root element identified by document.getElementById('root'). */
+
 createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
+  <StrictMode>   {/** `<StrictMode>` wraps the <App /> component to help us catch potential issues(error) early in the development process. */}
+     <App />     { /* `<App />` is the root component of your application, which is defined in App.jsx. */}
+    
   </StrictMode>,
 )
