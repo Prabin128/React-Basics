b77d7108796ce16d87fe676b4bb57208f361107c
Prabin <poudelprabin09@gmail.com>
2025-03-12 23:52:26 +0545

    react developer tools

---
+++ 9-React-DevTools/src/data/contact.js
@@ -0,0 +1,23 @@
+const testimonials = [
+    {
+        name: 'Senior Developer',
+        image: image1,
+        phone: '00000000000',
+        email: 'seniordev123@example.com',
+    },
+    {
+        name: 'Intermediate Developer',
+        image: image2,
+        phone: '4444444444',
+        email: 'intermediatedev123@example.com',
+    },
+    {
+        name: 'Junior Developer',
+        title: 'Software Developer Intern',
+        quote: 'I am so excited to learn this topic with the supervision of my senior and intermediate developer',
+        email: '',
+        image: image3,
+        isVerified: true,
+
+    }
+];
