e5e5568a334bf8ffaddae1385969637804260ba1
Prabin <poudelprabin09@gmail.com>
2025-05-20 22:43:28 +0545

    Stop watch using useEffect

--- 14-State-Management/14.2-React Hooks/14.2.4-useEffect-Practice/1-LiveTimer-StopWatch/README.md
+++ 14-State-Management/14.2-React Hooks/14.2.4-useEffect-Practice/1-LiveTimer-StopWatch/README.md
@@ -1,12 +1,32 @@
-# React + Vite
+# ⏱ Live Timer with useEffect
 
-This template provides a minimal setup to get React working in Vite with HMR and some ESLint rules.
+This is a simple live timer/stopwatch project built using **useEffect** hook.  
 
-Currently, two official plugins are available:
+---
 
-- [@vitejs/plugin-react](https://github.com/vitejs/vite-plugin-react/blob/main/packages/plugin-react) uses [Babel](https://babeljs.io/) for Fast Refresh
-- [@vitejs/plugin-react-swc](https://github.com/vitejs/vite-plugin-react/blob/main/packages/plugin-react-swc) uses [SWC](https://swc.rs/) for Fast Refresh
+## Features
 
-## Expanding the ESLint configuration
+- The timer starts counting only when the user clicks the **Start** button.
 
-If you are developing a production application, we recommend using TypeScript with type-aware lint rules enabled. Check out the [TS template](https://github.com/vitejs/vite/tree/main/packages/create-vite/template-react-ts) for information on how to integrate TypeScript and [`typescript-eslint`](https://typescript-eslint.io) in your project.
+- While running, it adds `1` every second using setInterval.
+
+- If the user clicks **Stop**, the timer pauses (we stop the interval).
+
+- If the user clicks **Reset**, the timer goes back to `0` and stops running.
+
+- When the component re-renders or unmounts, we **clear the interval** (important to prevent bugs or memory leaks).
+
+
+The concept is in:
+
+- Using `useState` to track the time and running state
+- Using `useEffect` to start/stop the timer based on `isRunning`
+---
+
+## React Concepts Used
+
+- `useState` — to manage the timer and start/stop state
+- `useEffect` — to start and clean up a timer interval
+- `setInterval` and `clearInterval` — for updating time every second
+
+---
--- 14-State-Management/14.2-React Hooks/14.2.4-useEffect-Practice/1-LiveTimer-StopWatch/src/App.jsx
+++ 14-State-Management/14.2-React Hooks/14.2.4-useEffect-Practice/1-LiveTimer-StopWatch/src/App.jsx
@@ -1,35 +1,43 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [seconds, setSeconds] = useState(0)   //Tracks the number of seconds
+
+  //state variable `isRunning` decides whether the timer should run. When true, timer starts
+  const [isRunning, setIsRunning] =useState(false); //Tracks wether the time is running
+
+  useEffect(()=> {  //We use useEffect to start the timer when isRunning becomes true.
+
+    let interval;
+
+    //If the timer is running, start the interval
+    if(isRunning){   //if true, We start a new interval using setInterval()
+      interval  =  setInterval(()=> {
+        setSeconds(prev => prev+1); //Increase time every 1 second (ake the previous second value and add 1)
+      }, 1000)
+    }
+    return () => {
+      clearInterval(interval);  //We use it to clear the timer so it doesn't keep running in the background.
+    };
+  }, [isRunning]);  //This tells React: Only run this useEffect when isRunning changes.
+    
+
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <div className="container">
+      <h2>Live Timer</h2>
+      <h1>{seconds} s</h1>
+
+      <div className="buttons">
+        <button onClick={() => setIsRunning(true)}>Start</button>  {/* Start: Sets isRunning to true} */}
+        <button onClick={() => setIsRunning(false)}>Stop</button>  {/* Stop: Sets isRunning to false} */}
+        <button onClick={() => { setIsRunning(false); setSeconds(0); }}>Reset</button> {/* Reset: Sets isRunning to false and sets seconds to 0*/}
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    </div>
+  );
 }
 
 export default App
