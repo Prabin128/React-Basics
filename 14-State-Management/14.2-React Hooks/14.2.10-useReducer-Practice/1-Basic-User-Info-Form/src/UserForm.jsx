ddf21b533c933cf18d1dd239cb56b4729a9edb69
Prabin <poudelprabin09@gmail.com>
2025-06-14 19:03:01 +0545

    Basic UserForm using useReducer

---
+++ 14-State-Management/14.2-React Hooks/14.2.10-useReducer-Practice/1-Basic-User-Info-Form/src/UserForm.jsx
@@ -0,0 +1,74 @@
+import { useReducer } from 'react'
+import './UserForm.css'
+import { useState } from 'react';
+
+const initialState = {name: '' , age: ''};
+
+const reducer = (state, action) => {
+  switch (action.type) {
+    case 'name':
+      return {...state, name:action.payload}
+    case 'age':
+      return {...state, age:action.payload}
+    case 'reset':
+      return initialState
+     
+    default:
+      return state;
+  }
+}
+
+function UserForm() {
+
+  const [isSubmitted, setIsSubmitted] = useState(false);
+
+  const [state, dispatch] = useReducer(reducer, initialState);
+
+
+  const handleName = (e) => {
+    dispatch({type: 'name', payload: e.target.value});
+  }
+
+  const handleAge = (e) => {
+    dispatch({type: 'age', payload: e.target.value});
+  }
+  const handleSubmit = () => {
+    console.log('Submitted:', state);
+    setIsSubmitted(true);
+  }
+  const handleReset = () => {
+    dispatch({type: 'reset'});
+    setIsSubmitted(true);
+  }
+
+  return (
+    <div className='main-div-container'>
+      <form className='form-container'>
+          <input 
+            className='input-field'
+            type='text'
+            value={state.name} 
+            placeholder='Enter the name' 
+            onChange={handleName}
+          />
+
+          <input
+            className='input-field'         
+            value={state.age} 
+            placeholder='Enter the age' 
+            onChange={handleAge}
+          />
+
+          <div className='button-group'>
+            <button className='btn submit-btn' onClick={handleSubmit}>Submit</button>
+            <button className='btn submit-btn' onClick={handleReset}>Reset</button>
+          </div>
+
+          {isSubmitted && (
+            <p className='output'>Hello, {state.name}. Your age is {state.age}</p>
+          )}
+      </form>
+    </div>
+  )}
+
+export default UserForm
